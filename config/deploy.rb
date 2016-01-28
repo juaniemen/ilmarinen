d93f893e1cf31a9e5657260f17e5d4577c7344f0
Juan Francisco Nieto Mendoza <juaniemen@hotmail.com>
2016-01-28 23:23:19 +0100

    Arreglo deploy.rb ¿No sé si final?

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,14 +23,14 @@ server '82.196.0.219/', port: 80, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'https://github.com/juaniemen/ilmarinen.git'
 set :application,     'ilmarinen'
-set :user,            'root'
+set :user,            'rails'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :development
+set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
